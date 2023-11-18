dce4ac05d0b39b32c3baf43092715c6b704be221
Emma Denton <emmad@192-168-1-110.tpgi.com.au>
2023-11-18 16:57:06 +1100

    adding database schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,33 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- Category model
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
+
+-- Product model
+CREATE TABLE Product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+-- Tag model
+CREATE TABLE Tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) NOT NULL
+);
+
+-- ProductTag model
+CREATE TABLE ProductTag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
